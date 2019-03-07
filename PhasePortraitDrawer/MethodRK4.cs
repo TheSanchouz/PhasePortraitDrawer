8a6e9ac37514a4debad993bdd56790ff74a9db0d
Alexander Trykin <thesanchouz@gmail.com>
2019-03-07 18:35:49 +0300

    Initial commit

---
+++ PhasePortraitDrawer/MethodRK4.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhasePortraitDrawer
+{
+    class MethodRK4
+    {
+        private readonly RPN function;
+        private readonly double x0;
+        private readonly double y0;
+    }
+}
